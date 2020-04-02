afa2bbff0e9b13902b5d3b2086ddde97690e7077
Blayze <blayzestone@protonmail.com>
2020-04-02 12:24:56 -0700

    Updates

---
+++ precache-manifest.14d8a7b6b93ca58ba1c2c92dea848997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0341b575ab43c1f519ce988c742fa43b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4d4a6e9f1f473f91b9e",
+    "url": "/monsters-rolodex/static/css/main.402278c0.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d4a6e9f1f473f91b9e",
+    "url": "/monsters-rolodex/static/js/main.b0101894.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
